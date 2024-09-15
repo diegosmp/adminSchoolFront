2eef71cd3675e9c5f53bd78a0d5f5b3a281699a5
diegosmp <diiegosmp@gmail.com>
2024-09-15 16:16:29 -0300

    initial front end

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-20 flex px-5 items-center justify-center md:justify-end md:text-base">
+      <span className="text-xs text-zinc-600">
+        Feito por{' '}
+        <a href="https://github.com/diegosmp" target="_blank" className="underline text-zinc-500">
+          Diêgo Sampaio
+        </a>
+      </span>
+    </footer>
+  )
+}
---
+++ app/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import logoSVG from '../../public/LogoAdminschool.svg'
+
+export default function Header() {
+  return (
+    <header className="w-full h-20 flex px-5 items-center justify-center md:justify-start 2xl:w-9/12">
+      <a href="/">
+        <Image alt="Logo site Admin School" src={logoSVG} priority width={163} />
+      </a>
+    </header>
+  )
+}
