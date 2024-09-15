a8b05e6b43a2712a1122a4e10995fed6d13c183a
diegosmp <diiegosmp@gmail.com>
2024-09-15 16:49:28 -0300

    style: footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-20 flex px-5 items-center justify-center md:justify-end md:text-base">
+    <footer className="w-full h-20 flex px-5 items-center justify-center md:justify-end md:text-base 2xl:w-9/12">
       <span className="text-xs text-zinc-600">
         Feito por{' '}
         <a href="https://github.com/diegosmp" target="_blank" className="underline text-zinc-500">
