9e8583b28575681cc69a0df10ebe0cac998611bf
diegosmp <diiegosmp@gmail.com>
2024-09-15 22:21:08 -0300

    style Sign up

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,15 @@
+import Footer from '../components/Footer'
 import Form from '../components/Form'
+import Header from '../components/Header'
 
 export default function Signup() {
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center">
-      <Form />
-    </main>
+    <div className="bg-zinc-950 h-screen w-full flex flex-col items-center">
+      <Header />
+      <main className="w-full h-screen flex flex-col items-center justify-center">
+        <Form />
+      </main>
+      <Footer />
+    </div>
   )
 }
